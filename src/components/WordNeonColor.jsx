7f7342710bd546b89dfb325b74d5fd6f7947e4ce
Lucenabo <alucena1002@gmail.com>
2024-11-29 01:16:28 +0100

    half page built

---
+++ src/components/WordNeonColor.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+import "../styles/global.css";
+
+WordNeonColor.propTypes = {
+  word: PropTypes.string.isRequired,
+  color: PropTypes.string,
+  effect: PropTypes.bool,
+};
+
+function WordNeonColor({ word, color = "rgba(128, 0, 128, 0.5)", effect }) {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const neonStyle = {
+    textShadow: isHovered
+      ? `0 0 10px ${color}, 0 0 10px ${color}, 0 0 10px ${color}, 0 0 10px ${color}`
+      : "none",
+    color: isHovered ? color : "inherit",
+  };
+
+  return (
+    <span
+      id="wordNeon"
+      class={effect ? "fade-in" : ""}
+      style={
+        effect ? neonStyle : { textShadow: `0 0 10px ${color}`, color: color }
+      }
+      onMouseEnter={() => effect && setIsHovered(true)}
+      onMouseLeave={() => effect && setIsHovered(false)}
+    >
+      {word}
+    </span>
+  );
+}
+
+export default WordNeonColor;
