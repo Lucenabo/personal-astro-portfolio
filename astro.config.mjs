1b91ea04daeb887658f76a4de0a1d67de0657b67
Lucenabo <alucena1002@gmail.com>
2024-12-03 00:29:46 +0100

    gitignore added

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,8 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://lucenabo.github.io",
+  base: "lucenabo.github.io",
   i18n: {
     defaultLocale: "en",
     locales: ["en", "es"],
